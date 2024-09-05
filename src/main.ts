bd660f881f745f1086ba99cd102e8aabc639e6a6
furkanogutcu <furkanogutcu.dev@gmail.com>
2024-09-05 16:51:10 +0300

    chore: improve app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.enableShutdownHooks();
+
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
+
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(3000);
 }
