def6411f35499a77e9656f474184c69a05eaa020
furkanogutcu <furkanogutcu.dev@gmail.com>
2024-11-03 13:06:04 +0300

    chore: move database folder to src

--- src/modules/common/database/database.module.ts
+++ src/modules/common/database/database.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import typeormDataSource from '../../../../database/typeorm.data-source';
+import typeormDataSource from '../../../database/typeorm.data-source';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeormDataSource.options)],
