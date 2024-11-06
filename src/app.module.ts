1cd410efe018d4be961cbd627dcc7fab3211c9cd
furkanogutcu <furkanogutcu.dev@gmail.com>
2024-11-06 13:17:52 +0300

    feat: app zod validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 
 import { GlobalExceptionFilter } from './common/exceptions/filters/global.exception-filter';
+import { AppZodValidationPipe } from './common/pipes/app-zod-validation.pipe';
 import { DatabaseModule } from './modules/common/database/database.module';
 import { ENVModule } from './modules/common/env/env.module';
 import { LoggerModule } from './modules/common/logger/logger.module';
 
 @Module({
   imports: [ENVModule, LoggerModule, DatabaseModule],
-  providers: [{ provide: APP_FILTER, useClass: GlobalExceptionFilter }],
+  providers: [
+    { provide: APP_PIPE, useClass: AppZodValidationPipe },
+    { provide: APP_FILTER, useClass: GlobalExceptionFilter },
+  ],
 })
 export class AppModule {}
