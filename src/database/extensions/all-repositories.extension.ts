4a9261bf6ec1991148e16c15d19fadb0d2c240ec
furkanogutcu <furkanogutcu.dev@gmail.com>
2024-11-06 14:01:24 +0300

    feat: database extensions

---
+++ src/database/extensions/all-repositories.extension.ts
@@ -0,0 +1,27 @@
+import { FindManyOptions, Repository } from 'typeorm';
+
+import { PaginatedAPIResponse } from '../../common/responses/types/api-response.type';
+
+declare module 'typeorm' {
+  interface Repository<Entity> {
+    listWithPagination(options: FindManyOptions<Entity>): Promise<PaginatedAPIResponse<Entity>>;
+  }
+}
+
+Repository.prototype.listWithPagination = async function (options): Promise<PaginatedAPIResponse<any>> {
+  const [data, total] = await this.findAndCount(options);
+
+  const per_page = options.take || data.length;
+
+  return {
+    metadata: {
+      pagination: {
+        page: options.skip ? Math.floor(options.skip / per_page) + 1 : 1,
+        per_page,
+        total_items: total,
+        total_pages: Math.ceil(total / per_page),
+      },
+    },
+    data,
+  };
+};
