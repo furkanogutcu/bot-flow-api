99f6edd9af49a71a7f2bcbe862fec52266ef475a
furkanogutcu <furkanogutcu.dev@gmail.com>
2024-12-29 11:48:45 +0300

    chore: remove unnecessary import

--- src/modules/common/logger/logger.module.ts
+++ src/modules/common/logger/logger.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 
-import { ENVModule } from '../env/env.module';
 import { ENVService } from '../env/env.service';
 
 @Module({
   imports: [
     PinoLoggerModule.forRootAsync({
-      imports: [ENVModule],
       inject: [ENVService],
       useFactory: async (envService: ENVService) => {
         const isDevMode = envService.get('NODE_ENV') === 'development';
